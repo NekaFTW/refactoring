76648f4dfd9df709032ad8f60ae3ee5e0ad5d637
NekaFTW <nknpardo@gmail.com>
2021-04-30 19:14:29 +0200

    Acabar 7 (Movimiento metodo)

--- refactoring/Client.java
+++ refactoring/Client.java
@@ -47,7 +47,7 @@ public class Client {
 		int bonificacions = 0;
 		String resultat = "Informe de lloguers del client " + getNom() + " (" + getNif() + ")\n";
 		for (Lloguer lloguer: lloguers) {
-		double quantitat = quantitatPerLloguer(lloguer);
+		double quantitat = lloguer.quantitat();
 
 		// afegeix lloguers freqüents
 			bonificacions ++;
@@ -65,27 +65,5 @@ public class Client {
 		// afegeix informació final
 		resultat += "Import a pagar: " + total + "€\n" + "Punts guanyats: " + bonificacions + "\n";
 		return resultat;
-	}
-	
-	public double quantitatPerLloguer(Lloguer lloguer) {
-		double quantitat = 0;
-		switch (lloguer.getVehicle().getCategoria()) {
-			case Vehicle.BASIC:
-				quantitat += 3;
-				if (lloguer.getDies() > 3) {
-					quantitat += (lloguer.getDies() - 3) * 1.5;
-				}
-				break;
-			case Vehicle.GENERAL:
-				quantitat += 4;
-				if (lloguer.getDies() > 2) {
-					quantitat += (lloguer.getDies() - 2) * 2.5;
-				}
-				break;
-			case Vehicle.LUXE:
-				quantitat += lloguer.getDies() * 6;
-				break;
-		}
-		return quantitat;
 	}
 }
